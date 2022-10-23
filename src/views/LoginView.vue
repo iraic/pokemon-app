3737496d640ed6186d79fe211aa69ddca854f137
Iraic <joseiraic@gmail.com>
2022-10-22 23:44:16 -0500

    validaciones

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -7,14 +7,19 @@
                     <v-card-title>Inicio de sesión</v-card-title>
                     <v-card-text>
                         <v-form @submit.prevent="login">
-                            <v-text-field label="Correo" placeholder="johndoe@gmail.com" type="email" v-model="email" />
+                            <v-text-field label="Correo" placeholder="johndoe@gmail.com" type="email"
+                                v-model="v$.emailUser.$model"/>
                             <v-text-field label="Contraseña" placeholder="6 caracteres" type="password"
-                                v-model="pass" />
+                                v-model="passUser" @blur="v$.passUser.$touch"/>
                             <v-btn block color="success" size="large" type="submit" variant="elevated">
                                 Entrar
                             </v-btn>
+                            
                         </v-form>
                     </v-card-text>
+                    <v-alert v-if="v$.emailUser.$error" type="error" variant="text">Error en la direccion de correo</v-alert>
+                    <v-alert v-if="v$.passUser.$error" type="error" variant="text">Error en la contraseña(mínimo 6 caracteres)</v-alert>
+                    <v-alert v-if="errorMsg.show" type="error" variant="text">{{errorMsg.msg}}</v-alert>
                     <v-card-actions>
                         <RouterLink to="/register">Registrarse</RouterLink>
                     </v-card-actions>
@@ -27,24 +32,44 @@
 <script>
 import { auth } from '@/firebase/firebaseConfig'
 import { signInWithEmailAndPassword } from "firebase/auth";
+import { useVuelidate } from '@vuelidate/core'
+import { required, email, minLength } from '@vuelidate/validators'
+
 export default {
     name: 'LoginView',
     data: () => ({
-        email: "",
-        pass: ""
+        emailUser: "",
+        passUser: "",
+        errorMsg: {show:false, msg:''},
 
     }),
     methods: {
-        login() {
-            signInWithEmailAndPassword(auth, this.email, this.pass)
+        async login() {
+            const isFormCorrect = await this.v$.$validate();
+            if(isFormCorrect){
+                signInWithEmailAndPassword(auth, this.emailUser, this.passUser)
                 .then(() => {
                     this.$router.replace('/')
                 })
-                .catch((error) => {
-                    alert(error.message);
+                .catch(() => {
+                    this.errorMsg.show=true;
+                    this.errorMsg.msg = "Error al iniciar sesión revise los datos";
                 });
+            }else{
+                this.errorMsg.show=true;
+                this.errorMsg.msg = "Llena los comapos con datos válidos";
+            }
         }
-    }
+    },
+    setup() {
+        return { v$: useVuelidate() }
+    },
+    validations() {
+        return {
+            emailUser: { required, email },
+            passUser: { required, minLength: minLength(6) },
+        }
+    },
 }
 </script>
   
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -8,16 +8,20 @@
                     <v-card-text>
                         <v-form @submit.prevent="userRegistration">
                             <v-text-field prepend-icon="mdi-email" label="Correo" placeholder="johndoe@gmail.com"
-                                type="email" v-model="email" />
+                                type="email" v-model="v$.emailUser.$model" />
                             <v-text-field prepend-icon="mdi-lock" label="Contraseña" placeholder="6 caracteres"
-                                type="password" v-model="pass" />
+                                type="password" v-model="passUser" @blur="v$.passUser.$touch"/>
                             <v-text-field prepend-icon="mdi-lock-check" label="Confirmar" placeholder="6 caracteres"
-                                type="password" v-model="confirm" />
+                                type="password" v-model="confirm" @blur="v$.confirm.$touch"/>
                             <v-btn block color="primary" size="large" type="submit" variant="elevated">
                                 Registrar
                             </v-btn>
                         </v-form>
                     </v-card-text>
+                    <v-alert v-if="v$.emailUser.$error" type="error" variant="text">Error en la direccion de correo</v-alert>
+                    <v-alert v-if="v$.passUser.$error" type="error" variant="text">Error en la contraseña(mínimo 6 caracteres)</v-alert>
+                    <v-alert v-if="v$.confirm.$error" type="error" variant="text">La confirmación debe ser identica a la primer contraseña</v-alert>
+                    <v-alert v-if="errorMsg.show" type="error" variant="text">{{errorMsg.msg}}</v-alert>
                     <v-card-actions>
                         <RouterLink to="/login">Ir a inicio de sesión</RouterLink>
                     </v-card-actions>
@@ -30,26 +34,46 @@
 <script>
 import { auth } from '@/firebase/firebaseConfig'
 import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { useVuelidate } from '@vuelidate/core'
+import { required, email, minLength, sameAs } from '@vuelidate/validators'
 
 export default {
     name: 'LoginView',
     data: () => ({
-        email: "",
-        pass: "",
-        confirm: ""
+        emailUser: "",
+        passUser: "",
+        confirm: "",
+        errorMsg: {show:false, msg:''},
 
     }),
     methods: {
-        userRegistration() {
+        async userRegistration() {
+            const isFormCorrect = await this.v$.$validate();
+            if(isFormCorrect){
             createUserWithEmailAndPassword(auth, this.email, this.pass)
                 .then(() => {
                     this.$router.replace('/home')
                 })
-                .catch((error) => {
-                    alert(error.message);
+                .catch(() => {
+                    this.errorMsg.show=true;
+                    this.errorMsg.msg = "Error al iniciar sesión revise los datos";
                 });
+            }else{
+                this.errorMsg.show=true;
+                this.errorMsg.msg = "Llena los comapos con datos válidos";
+            }
         }
-    }
+    },
+    setup() {
+        return { v$: useVuelidate() }
+    },
+    validations() {
+        return {
+            emailUser: { required, email },
+            passUser: { required, minLength: minLength(6) },
+            confirm: { sameAsPassword: sameAs(this.passUser) },
+        }
+    },
 }
 </script>
   
